fc3eb4a83e9085fa60f7d1df1c7282891b18a409
Martin Herbert <martin.herbert@mrh.cloud>
2018-05-25 14:22:24 +0100

    Updated constants code and removal of unused package

--- Constants.cs
+++ Constants.cs
@@ -4,6 +4,6 @@ namespace Amazon.Lambda.Function
 {
     public static class Constants
     {
-        public static readonly String AMISnapshotDescription = ""; 
+        public static readonly String AMISnapshotDescription = "Automated Backup - {0} ({1}) on {2}"; 
     }
 }
